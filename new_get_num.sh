250f73225d5172dcd9a510bf200750f9932ad198
Auriga-Xebec <Auriga-Xebec@gmail.com>
2022-02-02 11:27:24 +0800

    finished the new get numbers function

---
+++ new_get_num.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+print_error() 
+      
+{ 
+      
+    echo -e "\033[31mERROR:\033[0m $1" 
+      
+} 
+
+get_num()
+{
+    echo "enter a number between $1 and $2: "
+    read guess
+    while (( $guess < $1 || $guess > $2 )); do
+        print_error "enter a number between $1 and $2: "
+        read guess
+        
+    done
+    return $guess
+}
+
+get_num 1 10
+num=$?
+echo -e "\n$num" 
